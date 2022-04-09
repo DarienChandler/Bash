16366c761586e511c8d782bc828a46b52d88b60e
root <root@DESKTOP-HOB3O8I.localdomain>
2022-04-09 00:15:21 -0500

    04/09

---
+++ git_update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# This will automatically update the index of my web server. I am to cron tab the shell command to execute.
+git pull origin master
---
+++ notice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+echo "hi welcome"
+date  +"Date : %m/%d/%Y Time : %H.%M.%S"
